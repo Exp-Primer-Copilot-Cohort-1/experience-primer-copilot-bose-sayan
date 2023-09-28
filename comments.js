fb9b17820f12016266a55e21ab13170478268bc1
Sayan Bose <111633699+bose-sayan@users.noreply.github.com>
2023-09-28 05:48:26 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,30 +1,19 @@
-// Create a web server
-// 1. Create a web server
-// 2. Create a route for /comments
-// 3. Send back some static comments data
-// 4. Test your work!
-
-// 1. Create a web server
+// Create web server
 const express = require('express');
-const app = express();
-
-// 2. Create a route for /comments
-app.get('/comments', (req, res) => {
-    res.send('Here are some comments');
-});
-
-// 3. Send back some static comments data
-app.get('/comments', (req, res) => {
-    res.send([{
-        name: 'Andrew',
-        comment: 'This is my comment'
-    }, {
-        name: 'Jack',
-        comment: 'This is my comment'
-    }]);
-});
-
-// 4. Test your work!
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
-});
+const router = express.Router();
+// Import comments controller
+const commentsCtrl = require('../controllers/comments');
+// Import auth helper
+const auth = require('../middleware/auth');
+// Create comment
+router.post('/', auth, commentsCtrl.createComment);
+// Modify comment
+router.put('/:id', auth, commentsCtrl.modifyComment);
+// Delete comment
+router.delete('/:id', auth, commentsCtrl.deleteComment);
+// Get one comment
+router.get('/:id', auth, commentsCtrl.getOneComment);
+// Get all comments
+router.get('/', auth, commentsCtrl.getAllComments);
+// Export
+module.exports = router;
